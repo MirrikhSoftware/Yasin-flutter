54ec1778085cefe4b51608c135faa7f4a0035618
BahromjonPolat <itjunior.uz@gmail.com>
2022-08-31 21:02:56 +0500

    MyApp was replaced

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:yaaseen/screens/home/home_page.dart';
+import 'package:yaaseen/app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,14 +13,3 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Yosin Surasi',
-      home:  HomePage(),
-    );
-  }
-}
