a7f68cf366936e455a4dd05479ce74635a69ea84
BahromjonPolat <itjunior.uz@gmail.com>
2022-09-02 15:02:24 +0500

    bookmarks screen i ready

--- lib/screens/home/bookmarks.dart
+++ lib/screens/home/bookmarks.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:yaaseen/core/constants/app_strings.dart';
+import 'package:yaaseen/core/core.dart';
+import 'package:yaaseen/hive_helper/hive_boxes.dart';
+import 'package:yaaseen/models/verse/verse_model.dart';
+import 'package:yaaseen/widgets/widgets.dart';
 
 class BookmarksScreen extends StatelessWidget {
-  const BookmarksScreen({ Key? key }) : super(key: key);
+  const BookmarksScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: SimpleAppBar(title: AppStrings.bookmarks.tr()),
+      body: ValueListenableBuilder(
+        valueListenable: HiveBoxes.verseBox.listenable(),
+        builder: (context, Box<VerseModel> box, child) {
+          List<VerseModel> verses =
+              box.values.where((verse) => verse.isSaved).toList();
+          return Scrollbar(
+            // controller: _scrollController,
+            child: ListView.separated(
+              padding: EdgeInsets.only(bottom: 24.h),
+              // controller: _scrollController,
+              
+              itemCount: verses.length,
+              separatorBuilder: (ctx, i) => const Divider(),
+              itemBuilder: (context, index) {
+                VerseModel verse = verses[index];
+                return VerseListTile(verse: verse);
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:yaaseen/core/core.dart';
 import 'package:yaaseen/hive_helper/hive_boxes.dart';
 import 'package:yaaseen/models/verse/verse_model.dart';
 import 'package:yaaseen/widgets/widgets.dart';
-
+import 'dart:math' as math;
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -14,22 +14,19 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final ScrollController _scrollController = ScrollController();
 
-
   @override
   void initState() {
     super.initState();
 
-    HiveBoxes.prefsBox.listenable().addListener(() { 
-      setState(() {
-        
-      });
+    HiveBoxes.prefsBox.listenable().addListener(() {
+      setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: AppDrawer(),
+      drawer: const AppDrawer(),
       body: ValueListenableBuilder(
         valueListenable: HiveBoxes.verseBox.listenable(),
         builder: (context, Box<VerseModel> box, child) {
@@ -38,36 +35,19 @@ class _HomeScreenState extends State<HomeScreen> {
             physics: const BouncingScrollPhysics(),
             slivers: [
               const HomeAppBar(),
-
-              SliverToBoxAdapter(
-                child: Scrollbar(
-                  controller: _scrollController,
-                  child: ListView.separated(
-                    controller: _scrollController,
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: verses.length,
-                    separatorBuilder: (ctx, i) => const Divider(),
-                    itemBuilder: (context, index) {
-                      VerseModel verse = verses[index];
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    final int itemIndex = index ~/ 2;
+                    if (index.isEven) {
+                      VerseModel verse = box.getAt(itemIndex)!;
                       return VerseListTile(verse: verse);
-                    },
-                  ),
+                    }
+                    return Divider(thickness: 1.h, height: 24.h);
+                  },
+                  childCount: math.max(0, verses.length * 2 - 1),
                 ),
               ),
-              // SliverList(
-              //   delegate: SliverChildBuilderDelegate(
-              //     (context, index) {
-              //       final int itemIndex = index ~/ 2;
-              //       if (index.isEven) {
-              //         VerseModel verse = box.getAt(itemIndex)!;
-              //         return VerseListTile(verse: verse);
-              //       }
-              //       return Divider(thickness: 1.h, height: 24.h);
-              //     },
-              //     childCount: math.max(0, verses.length * 2 - 1),
-              //   ),
-              // ),
               SliverToBoxAdapter(child: SizedBox(height: 24.h))
             ],
           );
@@ -75,5 +55,4 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-
 }
